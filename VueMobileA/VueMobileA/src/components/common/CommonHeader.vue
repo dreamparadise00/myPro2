816683d8f9755a33e6ad1d49f06ee17abed626d9
乐园 <3110593207@qq.com>
2018-01-10 20:29:56 +0800

    2018.1.10

---
+++ VueMobileA/VueMobileA/src/components/common/CommonHeader.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class='header' :style="{background:bgColor}">
+      <slot>
+        <button @click='back'>{{nav}}</button>
+      </slot>
+      <h2>{{title}}</h2>
+  </div>
+</template>
+<script>
+     export default {
+         props  : ['title','bgColor','nav'],
+         methods: {
+             back(){
+                 history.go(-1);
+             }
+         }
+     }
+</script>
+<style>
+    .header{
+        height  : 1rem;
+        display : flex;
+        position: fixed;
+        top     : 0;
+        width   : 100%;
+    }
+    .header button{
+        width : 1rem;
+        height: 1rem;
+        color : #fff;
+        float : left;
+    }
+    .header h2{
+         float      : left;
+         color      : #fff;
+         text-align : center;
+         flex       : 1;
+         text-indent: -1rem;
+    }
+</style>
