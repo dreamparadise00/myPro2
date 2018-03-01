20657c52ce9758f2439a156bc9039b3917bcf9c2
dreamparadise00 <3110593207@qq.com>
2018-03-01 17:12:09 +0800

    2018.3.1

--- myBlog/es6/hello/dist/index.js
+++ myBlog/es6/hello/dist/index.js
@@ -2,3 +2,35 @@
 
 var a = 3;
 console.log(a);
+function paradise(first) {
+    for (var _len = arguments.length, arg = Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
+        arg[_key - 1] = arguments[_key];
+    }
+
+    var _iteratorNormalCompletion = true;
+    var _didIteratorError = false;
+    var _iteratorError = undefined;
+
+    try {
+        for (var _iterator = arg[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+            var val = _step.value;
+
+            console.log(val);
+        }
+    } catch (err) {
+        _didIteratorError = true;
+        _iteratorError = err;
+    } finally {
+        try {
+            if (!_iteratorNormalCompletion && _iterator.return) {
+                _iterator.return();
+            }
+        } finally {
+            if (_didIteratorError) {
+                throw _iteratorError;
+            }
+        }
+    }
+}
+
+paradise(0, 1, 2, 3, 4, 5, 6, 7); //输出8，8个数组元素
--- myBlog/es6/hello/src/index.js
+++ myBlog/es6/hello/src/index.js
@@ -1,2 +1,9 @@
 let a = 3;
 console.log(a);
+function paradise(first,...arg){
+    for(let val of arg){
+        console.log(val);
+    }
+}
+
+paradise(0,1,2,3,4,5,6,7); //输出8，8个数组元素
