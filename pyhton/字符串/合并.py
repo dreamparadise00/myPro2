50c94b8111f0abecd0a40a4aaa41a05427679410
dreamparadise00 <3110593207@qq.com>
2018-03-24 15:03:43 +0800

    2018.3.24

---
+++ pyhton/字符串/合并.py
@@ -0,0 +1,19 @@
+# what_he_does = " play ";
+# his_instrument = "guitar";
+# his_name = 'Rober Johnson';
+# artist_intro = his_name + what_he_does + his_instrument;
+#
+# print(artist_intro);
+
+# num = 1 ;
+# string = '1';
+# num2 = int(string);
+#
+# print(num + num2);
+
+num = "zhe shi 15 ge shuzi"     # 空格算一位
+num2 = 10;
+daChu = "nb!";
+result = daChu * (len(num) - num2);
+print(len(num));
+print(result);
