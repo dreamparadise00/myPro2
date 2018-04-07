96d92a37e9cfbc9f570ea3aecc5b42b9b1202c76
dreamparadise00 <3110593207@qq.com>
2018-04-07 13:38:10 +0800

    2018.4.7

--- pyhton/实战/pa.py
+++ pyhton/实战/pa.py
@@ -15,7 +15,13 @@ for item in nowplaying_movie_list:
             nowplaying_dict['name'] = tag_img_item['alt']
             nowplaying_list.append(nowplaying_dict)
 # print(nowplaying_list)
+requrl = 'https://movie.douban.com/subject/' + nowplaying_list[0]['id'] + '/comments' +'?' +'start=0' + '&limit=20'
+resp = request.urlopen(requrl)
+html_data = resp.read().decode('utf-8')
+soup = bs(html_data, 'html.parser')
+comment_div_lits = soup.find_all('div', class_='comment')
 eachCommentList = [];
 for item in comment_div_lits:
         if item.find_all('p')[0].string is not None:
             eachCommentList.append(item.find_all('p')[0].string)
+print(eachCommentList)
