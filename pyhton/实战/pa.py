77a8f5fc6dc079236a19b087569e8f7fdeeb24cf
dreamparadise00 <3110593207@qq.com>
2018-04-07 13:18:02 +0800

    2018.4.7

--- pyhton/实战/pa.py
+++ pyhton/实战/pa.py
@@ -6,4 +6,12 @@ from bs4 import BeautifulSoup as bs
 soup = bs(html_data, 'html.parser')
 nowplaying_movie = soup.find_all('div', id='nowplaying')
 nowplaying_movie_list = nowplaying_movie[0].find_all('li', class_='list-item')
-print(nowplaying_movie_list[0])
+# print(nowplaying_movie_list[0])
+nowplaying_list = []
+for item in nowplaying_movie_list:
+        nowplaying_dict = {}
+        nowplaying_dict['id'] = item['data-subject']
+        for tag_img_item in item.find_all('img'):
+            nowplaying_dict['name'] = tag_img_item['alt']
+            nowplaying_list.append(nowplaying_dict)
+print(nowplaying_list)
