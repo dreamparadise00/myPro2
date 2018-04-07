81b2273635090d82ccf8447f0161acb75748c20d
dreamparadise00 <3110593207@qq.com>
2018-04-07 13:11:57 +0800

    2018.4.7

---
+++ pyhton/实战/pa.py
@@ -0,0 +1,9 @@
+from urllib import request
+resp = request.urlopen('https://movie.douban.com/nowplaying/hangzhou/')
+html_data = resp.read().decode('utf-8')
+# print(html_data)
+from bs4 import BeautifulSoup as bs
+soup = bs(html_data, 'html.parser')
+nowplaying_movie = soup.find_all('div', id='nowplaying')
+nowplaying_movie_list = nowplaying_movie[0].find_all('li', class_='list-item')
+print(nowplaying_movie_list[0])
