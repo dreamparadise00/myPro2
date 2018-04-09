383b7083cd02b37ed6fe4efdc5208cf84e4be534
dreamparadise00 <3110593207@qq.com>
2018-04-09 21:41:10 +0800

    2018.4.9

--- pyhton/实战/pa.py
+++ pyhton/实战/pa.py
@@ -1,39 +1,55 @@
 from urllib import request
-resp = request.urlopen('https://movie.douban.com/chart')
+resp = request.urlopen('https://movie.douban.com/nowplaying/hangzhou/')
 html_data = resp.read().decode('utf-8')
 # print(html_data)
 from bs4 import BeautifulSoup as bs
 soup = bs(html_data, 'html.parser')
-top_movie = soup.find_all('tr', class_='item')
-top_movie_name = top_movie[0].find_all('a' , class_='nbg')
-top_movie_num = top_movie[0].find_all('div', class_='star clearfix')
-print(top_movie_name)
-# nowplaying_list = []
-# for item in nowplaying_movie_list:
-#         nowplaying_dict = {}
-#         nowplaying_dict['id'] = item['data-subject']
-#         for tag_img_item in item.find_all('img'):
-#             nowplaying_dict['name'] = tag_img_item['alt']
-#             nowplaying_list.append(nowplaying_dict)
-# # print(nowplaying_list)
-# requrl = 'https://movie.douban.com/subject/' + nowplaying_list[0]['id'] + '/comments' +'?' +'start=0' + '&limit=20'
-# resp = request.urlopen(requrl)
-# html_data = resp.read().decode('utf-8')
-# soup = bs(html_data, 'html.parser')
-# comment_div_lits = soup.find_all('div', class_='comment')
-# eachCommentList = []
-# for item in comment_div_lits:
-#         if item.find_all('p')[0].string is not None:
-#             eachCommentList.append(item.find_all('p')[0].string)
-# # print(eachCommentList)
-# comments = ''
-# for k in range(len(eachCommentList)):
-#     comments = comments + (str(eachCommentList[k])).strip()
-# # print(comments)
-# import re
-# # 清理数据中的标点符号
-# pattern = re.compile(r'[\u4e00-\u9fa5]+')
-# filterdata = re.findall(pattern, comments)
-# cleaned_comments = ''.join(filterdata)
+nowplaying_movie = soup.find_all('div', id='nowplaying')
+nowplaying_movie_list = nowplaying_movie[0].find_all('li', class_='list-item')
+# print(nowplaying_movie_list[0])
+nowplaying_list = []
+for item in nowplaying_movie_list:
+        nowplaying_dict = {}
+        nowplaying_dict['id'] = item['data-subject']
+        for tag_img_item in item.find_all('img'):
+            nowplaying_dict['name'] = tag_img_item['alt']
+            nowplaying_list.append(nowplaying_dict)
+# print(nowplaying_list)
+requrl = 'https://movie.douban.com/subject/' + nowplaying_list[0]['id'] + '/comments' +'?' +'start=0' + '&limit=20'
+resp = request.urlopen(requrl)
+html_data = resp.read().decode('utf-8')
+soup = bs(html_data, 'html.parser')
+comment_div_lits = soup.find_all('div', class_='comment')
+eachCommentList = []
+for item in comment_div_lits:
+        if item.find_all('p')[0].string is not None:
+            eachCommentList.append(item.find_all('p')[0].string)
+# print(eachCommentList)
+comments = ''
+for k in range(len(eachCommentList)):
+    comments = comments + (str(eachCommentList[k])).strip()
+# print(comments)
+import re
+# 清理数据中的标点符号
+pattern = re.compile(r'[\u4e00-\u9fa5]+')
+filterdata = re.findall(pattern, comments)
+cleaned_comments = ''.join(filterdata)
 # print(cleaned_comments)
 
+import jieba  # jieba分词包
+import pandas as pd
+segment = jieba.lcut(cleaned_comments)
+words_df = pd.DataFrame({'segment': segment})
+# print(words_df)
+
+stopwords=pd.read_csv("stop_words_zh.txt",index_col=False,quoting=3,sep="\t",names=['stopword'], encoding='utf-8')#quoting=3全不引用
+words_df=words_df[~words_df.segment.isin(stopwords.stopword)]
+# print(words_df)
+
+#词频统计
+import numpy    #numpy计算包
+words_stat=words_df.groupby(by=['segment'])['segment'].agg({"计数":numpy.size})
+words_stat=words_stat.reset_index().sort_values(by=["计数"],ascending=False)
+print(words_stat)
+
+print(1)
